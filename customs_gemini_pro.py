7bf445c14f68eabf7a5e685cd3ef865e0e5a754d
thusarawijesena-bit <thusarawijesena@gmail.com>
2026-04-12 23:01:29 +0530

    Update customs_gemini_pro.py

--- customs_gemini_pro.py
+++ customs_gemini_pro.py
@@ -95,7 +95,7 @@ if st.button("🔍 සම්පූර්ණ රේගු වාර්තාව �
                     """
 
                     # මොඩල් එකේ නම 'gemini-pro' විදිහට වෙනස් කළා (404 Error එක හදන්න)
-                    model = genai.GenerativeModel('gemini-3-flash')
+                    model = genai.GenerativeModel('gemini-2.5-flash')
                     response = model.generate_content(ai_prompt)
 
                     st.markdown(f'<div class="report-box">{response.text}</div>', unsafe_allow_html=True)
